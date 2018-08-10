97f4e9900304d588af9d9ee3a5e0dfe5545a0163
Hannes Schütz <hannes@fam-schuetz.de>
2018-08-11 01:40:52 +0200

    add PWA Support

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,18 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
-  ],
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Setlist Composer",
+        short_name: "Setlist Composer",
+        start_url: "/",
+        background_color: "#f5f5f5",
+        theme_color: "#90D4A8",
+        display: "standalone",
+        //icon: "./src/images/favicon.png", // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+    ],
 }
